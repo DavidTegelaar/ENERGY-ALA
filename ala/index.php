5705d4c52ad7ea738ae7a80e1d50fa3fe8aa4cb2
DavidTegelaar <74899056+DavidTegelaar@users.noreply.github.com>
2021-03-11 12:58:06 +0100

    Initial Files 1

---
+++ ala/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="language" content="NL">
+    <meta name="description" content="Gainergy, Energy, Drink">
+    <meta name="keywords" content="Gainergy, Energy, Drink">
+    <meta name="author" content="David Tegelaar">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gainergy</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+
+<body>
+  
+    <header>
+        <article class="item-logo">
+            <img src="pictures/logo.png" height="130" width="180">
+        </article>
+
+        <article class="item_nav">
+            <ul>
+                <li><a href="index.php">Homepage</a></li>
+                <li><a href="#main-box3">Product</a></li>
+                <li><a href="#main-box3">Over ons</a></li>
+                <li><a href="#main-box3">Goede Doelen</a></li>
+                <li><a href="#main-box3">FAQ</a></li>
+                <li><a href="Contact.html">Contact<,/a></li>
+            </ul>
+        </article>
+
+    </header>
+
+    <main>
+        <article class="Product_Slideshow">
+         <div class="w3-content w3-section" style="max-width:500px">
+           <img class="mySlides" src="pictures/Can Template Original Flavor.png" style="width:100%">
+           <img class="mySlides" src="pictures/Can Template Coffee.png" style="width:100%">
+           <img class="mySlides" src="pictures/Can Template Blueberry.png" style="width:100%">
+         </div>
+
+         <script>
+         var myIndex = 0;
+         carousel();
+
+        function carousel() {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 2500); // Change image every 2 seconds
+        }
+        </script>
+        </article>
+
+
+
+
+
+        <article class="Data_Locatie">
+    <?php  
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $database = "energy";
+    $conn = new mysqli($host, $user, $pass, $database);
+    if ($conn->connect_error) {
+        echo "verbinding mislukt";
+    }
+
+    $sql = "SELECT plaatsnaam FROM locaties";
+    if ($result = $conn->query($sql)) {
+        while ($row = $result->fetch_row()) {
+            echo "<section class='locatie'>" . $row[0] . "</section>";
+        }
+
+    }
+    $result->close();
+    $conn->close();
+    
+    
+    ?>
+        </article>
+    </main>
+    
+
+</body>
