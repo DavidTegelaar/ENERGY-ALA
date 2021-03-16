9ecb6c6f2e6bfde4ea5e04fa8d563792de62c445
DavidTegelaar <74899056+DavidTegelaar@users.noreply.github.com>
2021-03-16 12:32:33 +0100

    index

--- index.php
+++ index.php
@@ -32,38 +32,57 @@
 
     </header>
 
-    <main>
-        <article class="Product_Slideshow">
-         <div class="SlideFoto" style="max-width:500px">
-           <img class="mySlide" src="ala/pictures/Can Template Original Flavor.png" style="width:100%">
-           <img class="mySlide" src="ala/pictures/Can Template Coffee.png" style="width:100%">
-           <img class="mySlide" src="ala/pictures/Can Template Blueberry.png" style="width:100%">
-         </div>
 
-         <script>
-         var myIndex = 0;
-         carousel();
+<div class="slideshow-container">
 
-        function carousel() {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-        }
-        myIndex++;
-        if (myIndex > x.length) {myIndex = 1}    
-        x[myIndex-1].style.display = "block";  
-        setTimeout(carousel, 2500); // Change image every 2.5 seconds
-        }
-        </script>
-        </article>
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="ala/pictures/Can Template Original Flavor.png" style="width:50%">
+  <div class="text">Original</div>
+</div>
 
-        <article class="Home-Evenementen">
-            
-        </article>
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="ala/pictures/Can Template Coffee.png" style="width:50%">
+  <div class="text">Coffee</div>
+</div>
 
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="ala/pictures/Can Template Blueberry.png" style="width:50%">
+  <div class="text">Blueberry</div>
+</div>
 
+</div>
+<br>
 
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2500); // Change image every 2 seconds
+}
+</script>
 
 
     <article class="Data_Locatie">
@@ -77,15 +96,15 @@
         echo "verbinding mislukt";
     }
 
-    /* $sql = "SELECT datum FROM evenementen order by datum LIMIT 0,3";
+    $sql = "SELECT datum FROM evenementen order by datum LIMIT 0,3";
     if ($result = $conn->query($sql)) {
         while ($row = $result->fetch_row()) {
             echo "<section class='locatie'>" . $row[0] . "</section>";
-        } */
+        } 
 
-    /* }
+    }
     $result->close();
-    $conn->close(); */
+    $conn->close(); 
     
     
     ?>
