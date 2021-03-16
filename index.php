2f26f6e124b406d230c3666cd8b2216f32696fc8
DavidTegelaar <74899056+DavidTegelaar@users.noreply.github.com>
2021-03-16 11:18:00 +0100

    update

--- index.php
+++ index.php
@@ -85,7 +85,7 @@
 
     }
     $result->close();
-    $sql = "SELECT locatie_id FROM evenementen INNER JOIN locaties ON locatie.id = locaties.plaatsnaam ORDER BY datum ASC;";
+    $sql = "SELECT locatie_id FROM evenementen;";
     if ($result = $conn->query($sql)) {
         while ($row = $result->fetch_row()) {
             echo "<section class='Plaats'>" . $row[0] . "</section>";
